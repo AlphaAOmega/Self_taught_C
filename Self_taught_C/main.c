89fa46ae018c5fae723653e8c2eab8a71ddeb6a5
AlphaAOmega <france998811@gmail.com>
2020-12-29 00:11:53 +0900

    Create a project

---
+++ Self_taught_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Self_taught_C
+//
+//  Created by 하동훈 on 2020/12/29.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
